ccf689f7516268090be6b27ae9b5b3dde7015636
rajeshkanna55 <rkathiravan889@gmail.com>
2022-10-05 10:32:17 +0530

    changes cod

--- login.js
+++ login.js
@@ -5,27 +5,52 @@ function validate(){
     var Array=JSON.parse(localStorage.getItem('password'));
     if(uname != '' && pword != '')
     {
-        // alert('password correct');
-       for(var i=0;i<=Arr.length;i++)
-          {
-              for(var j=0;j<=Array.length;j++)
-                 {
-                  if(uname==Arr[i] && pword==Array[j])
-                  {
-                   alert('succes');
-                  }
-                 
-                 }
-                }
-                  if(uname!=Arr[i] && pword !=Array[j])
-                  {
-                    alert('invalid username or password');
-                  }
-                    
-             
-    } 
-    else
+      var i=0;
+        var j=0;
+        console.log(i);
+      do{
+        
+        if(Arr[i]==uname && Array[j]==pword){
+          var user=Arr[i]
+          var pass=Array[j]
+          alert('success')
+        }
+      // else{
+          // alert('invalid username or password')
+      // }
+        i++;
+        j++;
+        
+      }while(i<=Arr.length,j<=Array.length);
+           
+      
+
+      
+      if(user!=uname && pass!=pword){
+        alert('invalid username or password')
+      }
+
+      }
+
+      //  for(var i=0;i<=Arr.length;i++)
+      //     {
+      //         for(var j=0;j<=Array.length;j++)
+      //            {
+      //              if(uname==Arr[i] && pword==Array[j])
+      //               {
+      //                alert('succes');
+      //               }
+      //             }
+      //           }  
+      //              {
+      //                   alert('invalid username or password')
+      //               }
+                               
+    
+      //   } 
+    else 
        {
-        alert('please enter the username & password..!')
+        alert('please enter the username & password..!');
        }
+       
   }
